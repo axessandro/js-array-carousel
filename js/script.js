e4f8f416f32e061fd98ab41bc5bd79c5d55bbbab
Alessandro Sartor <alessandro.sartor.1999@gmail.com>
2022-10-12 17:01:48 +0200

    Carousel script

--- js/script.js
+++ js/script.js
@@ -9,21 +9,34 @@ const imgs = [
 const dwnBtn = document.querySelector(".dwn-btn");
 const upBtn = document.querySelector(".up-btn");
 const slider = document.querySelector(".ms_slider");
+const carousel = document.querySelector(".ms_carousel");
 
 // Read array witch cycle
 for (let i = 0; i < imgs.length; i++){
-    const thisImg =imgs[i];
+    const thisImg = imgs[i];
+    // main img
     const img = `
         <div class="ms_item">
             <img src="${thisImg}" alt="">
         </div>`;
     slider.innerHTML += img;
+    // carousel
+    const carouselImg = `
+        <div class="img_wrapper">
+            <img src="${thisImg}" alt="">
+        </div>
+    `
+    carousel.innerHTML += carouselImg;
 }
 
 // Start status
 const items = document.getElementsByClassName("ms_item");
 let sliderPosition = 0;
 items[sliderPosition].classList.add("active");
+// Start carousel status
+const carouselItems = document.getElementsByClassName("img_wrapper")
+let carouselPosition = 0;
+carouselItems[carouselPosition].classList.add("active-carousel")
 
 // Navigation
 // dwnBtn at click
@@ -31,11 +44,34 @@ dwnBtn.addEventListener("click", function(){
     if (sliderPosition < imgs.length -1) {
         // Remove .active to sliderPosition
         items[sliderPosition].classList.remove("active");
+        // Remove .active-carousel to carouselPosition  
+        carouselItems[carouselPosition].classList.remove("active-carousel");
+
         // sliderPosition + 1
         sliderPosition++;
+        // carouselPosition +1
+        carouselPosition++;
+
         // Add .active to next sliderPosiiton
         items[sliderPosition].classList.add("active");
-    } 
+        // Add .active-carousel to next carouselPosition
+        carouselItems[carouselPosition].classList.add("active-carousel")
+    } else {
+        // Remove .active to sliderPosition
+        items[sliderPosition].classList.remove("active");
+        // Remove .active-carousel to carouselPosition  
+        carouselItems[carouselPosition].classList.remove("active-carousel");
+
+        // sliderPosition = 0
+        sliderPosition = 0;
+        // carouselPosition = 0
+        carouselPosition = 0;
+
+        // Add .active to next sliderPosiiton
+        items[sliderPosition].classList.add("active");
+        // Add .active-carousel to next carouselPosition
+        carouselItems[carouselPosition].classList.add("active-carousel")
+    }
 })
 
 // upBtn at click
@@ -43,10 +79,32 @@ upBtn.addEventListener("click", function(){
     if (sliderPosition > 0) {
         // Remove .active to sliderPosition
         items[sliderPosition].classList.remove("active");
+        // Remove .active-carousel to carouselPosition  
+        carouselItems[carouselPosition].classList.remove("active-carousel");
+
         // sliderPosition - 1
         sliderPosition--;
+        // carouselPosition - 1
+        carouselPosition--;
+
         // Add .active to next sliderPosiiton
         items[sliderPosition].classList.add("active");
+        // Add .active-carousel to next carouselPosition
+        carouselItems[carouselPosition].classList.add("active-carousel")
+    } else {
+        // Remove .active to sliderPosition
+        items[sliderPosition].classList.remove("active");
+        // Remove .active-carousel to carouselPosition  
+        carouselItems[carouselPosition].classList.remove("active-carousel");
+
+        // sliderPosition = imgs.length
+        sliderPosition = imgs.length -1;
+        // carouselPosition = imgs.length
+        carouselPosition = imgs.length -1;
+
+        // Add .active to next sliderPosiiton
+        items[sliderPosition].classList.add("active");
+        // Add .active-carousel to next carouselPosition
+        carouselItems[carouselPosition].classList.add("active-carousel")
     } 
-    
 })
